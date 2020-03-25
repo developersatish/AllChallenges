e5e06fc71b2c9d1b35b805c7395f452d05d0a220
Satish Singh <satish.singh@barco.com>
2020-03-25 19:27:45 +0530

    inprogres

--- AllCallenges/30DaysCodes/BinaryNumbers.cs
+++ AllCallenges/30DaysCodes/BinaryNumbers.cs
@@ -12,9 +12,16 @@ namespace AllChallenges._30DaysCodes
 
             // https://stackoverflow.com/questions/2954962/convert-integer-to-binary-in-c-sharp/2955001
             string result = Convert.ToString(Convert.ToInt32(Convert.ToString(n), 10), 2);
-
-
-            Console.WriteLine(result);
+            int j=0;
+            string temp=string.Empty;
+            for (int i = 0; i < result.Length; i++)
+            {
+                if(!String.IsNullOrEmpty(temp)){
+                    temp=result[i].ToString();
+                }                
+                Console.WriteLine(result[i]);
+            }
+            //Console.WriteLine(result);
         }
     }
 }
